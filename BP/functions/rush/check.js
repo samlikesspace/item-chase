d7983a24db38d6f9b31d0548823517166e2eb47a
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-08 23:11:30 +1100

    Update check.js

--- BP/functions/rush/check.js
+++ BP/functions/rush/check.js
@@ -1,4 +1,4 @@
 ## Check >> Item
-execute if score if entity @a[tag=runner,hasitem={item=flint_and_steel}] run function rush/win
-execute if score if entity @a[tag=runner,hasitem={item=shears}] run function rush/win
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run function rush/win
+execute if score item rushDB 2 if entity @a[tag=runner,hasitem={item=shears}] run function rush/win
 ## Check >> Death
