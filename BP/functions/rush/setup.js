e99b76de22ff3e2d4c96cf58ba3376e54853142c
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-08 20:48:30 +1100

    Update and rename setup.js to setup.js

--- BP/functions/rush/setup.js
+++ BP/functions/rush/setup.js
@@ -1,3 +1,4 @@
 tellraw @a {"rawtext":[{"text":"§l§4Warning! §r§cThis may remove blocks placed at the coordinates: 0, 0, 0!"}]}
 scoreboard objectives add bDB dummy
 structure load setup 0 0 0
+function rush/help
