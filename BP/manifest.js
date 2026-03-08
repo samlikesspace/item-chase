251d0781c12c08e81413fc7d32974565c8763a1f
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-08 20:50:16 +1100

    Update manifest.js

--- BP/manifest.js
+++ BP/manifest.js
@@ -2,7 +2,7 @@
 	"format_version": 2,
 	"header": {
 		"name": "§l§sItem Rush§r",
-		"description": "§l§sWelcome!§r\n\nHaving issues? Use §s/function help§r",
+		"description": "§l§sWelcome! ITEM RUSH§r is a fun 2+ player game.\nRace to collect your item before the hunters catch you!§r\n\nHaving issues? Use §s/function help§r",
 		"uuid": "52f4217d-7bc4-484d-992b-a9b78c4cfcda",
 		"version": [1, 0, 0],
 		"min_engine_version": [1, 20, 0]
