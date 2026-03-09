1822a059b7bcc4515af0f6a1d5a0e72bc0bedc01
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-09 20:48:18 +1100

    Update manifest.js

--- BP/manifest.js
+++ BP/manifest.js
@@ -5,7 +5,6 @@
 		"description": "§l§sWelcome! ITEM RUSH§r is a fun 2+ player game. Race to collect your item before the hunters catch you!\n§rHaving issues? Use §s/function help§r",
 		"uuid": "8aa93a45-22a7-4814-a3cf-7b7a5c9f2c79",
 		"version": [1, 0, 0],
-		"min_engine_version": [1, 20, 0]
 	},
 	"modules": [
 		{
