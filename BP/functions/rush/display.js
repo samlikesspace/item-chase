561cce70e544f0ad87574d3abde3ae28002ab933
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-28 23:37:23 +1000

    Update display.js

--- BP/functions/rush/display.js
+++ BP/functions/rush/display.js
@@ -1,11 +1,14 @@
-execute if score stage rushDB matches 1 if score item rushDB matches 1 run tititleraw @a title {"rawtext":[{"text":"§lAPPLE"}]}
-execute if score stage rushDB matches 1 if score item rushDB matches 2 run tititleraw @a title {"rawtext":[{"text":"§lDANDILION"}]}
-execute if score stage rushDB matches 1 if score item rushDB matches 3 run tititleraw @a title {"rawtext":[{"text":"§lSHEARS"}]}
-execute if score stage rushDB matches 1 if score item rushDB matches 4 run tititleraw @a title {"rawtext":[{"text":"§lFLINT & STEEL"}]}
-execute if score stage rushDB matches 1 if score item rushDB matches 5 run tititleraw @a title {"rawtext":[{"text":"§lSTONE STAIR"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 1 run titleraw @a title {"rawtext":[{"text":"§lAPPLE"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 2 run titleraw @a title {"rawtext":[{"text":"§lDANDILION"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 3 run titleraw @a title {"rawtext":[{"text":"§lSHEARS"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 4 run titleraw @a title {"rawtext":[{"text":"§lFLINT & STEEL"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 5 run titleraw @a title {"rawtext":[{"text":"§lSTONE STAIR"}]}
 
-
-execute if score stage rushDB matches 2 if score item rushDB matches 5 run tititleraw @a actionbar {"rawtext":[{"text":"§lItemSTONE STAIR"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 1 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lAPPLE"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 2 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lDANDILIOM"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 3 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lSHEARS"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 4 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lFLINT & STEEL"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 5 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lSTONE STAIR"}]}
 
 
 execute if score item rushDB matches 6 run scoreboard players set item rushDB 0
