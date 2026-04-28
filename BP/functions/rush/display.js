9faff846ce9daf601e1df25c98d0e7f1ebf6016f
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 00:03:37 +1000

    Update display.js

--- BP/functions/rush/display.js
+++ BP/functions/rush/display.js
@@ -4,7 +4,7 @@ execute if score stage rushDB matches 1 if score item rushDB matches 3 run title
 execute if score stage rushDB matches 1 if score item rushDB matches 4 run titleraw @a title {"rawtext":[{"text":"§lFLINT & STEEL"}]}
 execute if score stage rushDB matches 1 if score item rushDB matches 5 run titleraw @a title {"rawtext":[{"text":"§lSTONE STAIR"}]}
 
-execute if score stage rushDB matches 2 if score item rushDB matches 1 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"APPLE | §l§a"},{"selector": "@a[tag=runner]"},{"text":"§r§o§7(you)§r§7 | "},{"selector": "@a[tag=hunter]"}]}
+execute if score stage rushDB matches 2 if score item rushDB matches 1 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"APPLE  §l§a"},{"selector": "@a[tag=runner]"},{"text":" §r§o§7(you)§r§7  §a"},{"selector": "@a[tag=hunter]"}]}
 execute if score stage rushDB matches 2 if score item rushDB matches 2 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"DANDILION"}]}
 execute if score stage rushDB matches 2 if score item rushDB matches 3 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lSHEARS"}]}
 execute if score stage rushDB matches 2 if score item rushDB matches 4 run titleraw @a[tag="runner"] actionbar {"rawtext":[{"text":"§lFLINT & STEEL"}]}
