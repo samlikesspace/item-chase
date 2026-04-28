2d3ae89c2460423c9fbf1b8ad4d88b9d61775ffe
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-28 23:10:15 +1000

    Update and rename displays.js to display.js

--- BP/functions/rush/display.js
+++ BP/functions/rush/display.js
@@ -3,4 +3,9 @@ execute if score stage rushDB matches 1 if score item rushDB matches 2 run titit
 execute if score stage rushDB matches 1 if score item rushDB matches 3 run tititleraw @a title {"rawtext":[{"text":"§lSHEARS"}]}
 execute if score stage rushDB matches 1 if score item rushDB matches 4 run tititleraw @a title {"rawtext":[{"text":"§lFLINT & STEEL"}]}
 execute if score stage rushDB matches 1 if score item rushDB matches 5 run tititleraw @a title {"rawtext":[{"text":"§lSTONE STAIR"}]}
+
+
+execute if score stage rushDB matches 2 if score item rushDB matches 5 run tititleraw @a actionbar {"rawtext":[{"text":"§lItemSTONE STAIR"}]}
+
+
 execute if score item rushDB matches 6 run scoreboard players set item rushDB 0
