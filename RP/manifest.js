6212dce8461a5546dd7d2070bdcc41d3e8f5c5bd
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 18:15:19 +1000

    Update manifest.js

--- RP/manifest.js
+++ RP/manifest.js
@@ -2,7 +2,7 @@
 	"format_version": 2,
 	"header": {
 		"name": "§l§7Item Chase§r",
-		"description": "§l§6(!) Note: §r§gThis texture pack is §l§onot§r required, just highly recogmended for a better experience!",
+		"description": "§l§6(!) Note: §r§gThis texture pack is §l§onot§r required, just highly recommended for a better experience!",
 		"uuid": "86278ebd-9e11-4034-8e2d-b2154349aed3",
 		"version": [1, 0, 0],
 		"min_engine_version": [1, 20, 0]
