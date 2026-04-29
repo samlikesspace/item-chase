c2a8442963d5228e423ad49fbd0a3e2b008b9e00
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 18:12:43 +1000

    Update manifest.js

--- BP/manifest.js
+++ BP/manifest.js
@@ -1,8 +1,8 @@
 {
 	"format_version": 2,
 	"header": {
-		"name": "§l§sItem Rush§r",
-		"description": "§l§sWelcome! ITEM RUSH§r is a fun 2+ player game. Race to collect your item before the hunters catch you!\n§rHaving issues? Use §s/function help§r",
+		"name": "§l§7Item Chase§r",
+		"description": "§l§2:) §r§aThanks for download! You can use §2/function rush/start§a to start a game!",
 		"uuid": "8aa93a45-22a7-4814-a3cf-7b7a5c9f2c79",
 		"version": [1, 0, 0],
 		"min_engine_version": [1, 20, 0]
