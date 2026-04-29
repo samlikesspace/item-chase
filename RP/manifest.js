1cdb2a68a98201546e8d9a8bd96e3fc0dafff88b
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 18:06:59 +1000

    Create manifest.js

---
+++ RP/manifest.js
@@ -0,0 +1,18 @@
+{
+	"format_version": 2,
+	"header": {
+		"name": "§l§sItem Rush§r",
+		"description": "§l§sWelcome! ITEM RUSH§r is a fun 2+ player game. Race to collect your item before the hunters catch you!\n§rHaving issues? Use §s/function help§r",
+		"uuid": "8aa93a45-22a7-4814-a3cf-7b7a5c9f2c79",
+		"version": [1, 0, 0],
+		"min_engine_version": [1, 20, 0]
+	},
+	"modules": [
+		{
+			"description": "resources",
+			"version": [1, 0, 0],
+			"uuid": "c11de555-16ec-4f81-8306-59486517f2c7",
+			"type": "data"
+		}
+	]
+}
