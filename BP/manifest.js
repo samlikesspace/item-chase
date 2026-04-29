e0164798c7620693e1d585a2de506b3baecaecf9
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 18:17:13 +1000

    Update manifest.js

--- BP/manifest.js
+++ BP/manifest.js
@@ -2,7 +2,7 @@
 	"format_version": 2,
 	"header": {
 		"name": "§l§7Item Chase§r",
-		"description": "§l§2:) §r§aThanks for download! You can use §2/function rush/start§a to start a game!",
+		"description": "§l§2:) §r§aThanks for download! You can use §2/function i/start§a to start a game!",
 		"uuid": "8aa93a45-22a7-4814-a3cf-7b7a5c9f2c79",
 		"version": [1, 0, 0],
 		"min_engine_version": [1, 20, 0]
